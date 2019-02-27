6d2095393f4d4c112c81a9bd1c2630fd12c977c1
Linette V Maliakal <lmaliakal21@gmail.com>
2019-02-26 20:20:16 -0600

    attempt at taking text input to appear on canvas
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+function getTweet(){
+  var txt = document.getElementById("field1");
+  return txt;
+}
+
 var context;
 var text = "";
 var textDirection ="";
@@ -7,38 +12,35 @@ $(function() {
   setInterval("animate()", 30);
   textDirection ="right";
   textXpos = 5;
-  text = "Animation!";
+  text = "#tweet"//document.getElementById("field1");
 });
-        function animate() {
-            // Clear screen
-            context.clearRect(0, 0, 500, 500);
-            context.globalAlpha = 1;
-            context.fillStyle = '#fff';
-            context.fillRect(0, 0, 500, 500);
+function animate() {
+// Clear screen
+  context.clearRect(0, 0, 500, 500);
+  context.globalAlpha = 1;
+  context.fillStyle = '#fff';
+  context.fillRect(0, 0, 500, 500);
 
-            var metrics = context.measureText(text);
-            var textWidth = metrics.width;
+  var metrics = context.measureText(text);
+  var textWidth = metrics.width;
 
-            if (textDirection == "right") {
-                textXpos += 10;
-
-                if (textXpos > 500 - textWidth) {
-                    textDirection = "left";
-                }
-            }
-            else {
-                textXpos -= 10;
-
-                if (textXpos < 10) {
-                    textDirection = "right";
-                }
-            }
-
-            context.font = '20px _sans';
-            context.fillStyle = '#FF0000';
-            context.textBaseline = 'top';
-            context.fillText  ( text, textXpos, 180);
-          }
+  if (textDirection == "right") {
+    textXpos += 10;
+    if (textXpos > 500 - textWidth) {
+      textDirection = "left";
+    }
+  }
+  else {
+    textXpos -= 10;
+    if (textXpos < 10) {
+      textDirection = "right";
+    }
+  }
+  context.font = '20px _sans';
+  context.fillStyle = '#FF0000';
+  context.textBaseline = 'top';
+  context.fillText  ( text, textXpos, 180);
+}
 /* var _ref;
 var _createClass = function () {
   function defineProperties(target, props) {
