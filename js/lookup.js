9cbf3e97237b9049f0fc68e760843bc3e6f9857b
macabrera9 <mcabrera2@luc.edu>
2019-03-01 18:43:50 -0600

    can now add manual input to textboxes

--- js/lookup.js
+++ js/lookup.js
@@ -44,9 +44,9 @@ function getKeys(obj, val) {
     return objects;
 }
 //temporary json file for testing, will be json file obtained from backend server
-var json = '{"name":"Linette", "age":31, "city":"Chicago"}'; //'{"glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","ID":"44","str":"SGML","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}';
+/*var json = '{"name":"Linette", "age":31, "city":"Chicago"}'; //'{"glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","ID":"44","str":"SGML","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}';
 var js = JSON.parse(json);
-document.getElementById("tweetElement").innerHTML = js.city;
+//document.getElementById("tweetElement").innerHTML = js.city;
 
 console.log(getObjects(js,'ID',''));
 //returns 2 objects since keys with name ID are found in 2 objects
@@ -54,112 +54,13 @@ console.log(getObjects(js,'ID',''));
 //grabbing keys through searching values in JSON
 console.log(getKeys(js,'SGML'));
 //returns array ["ID", "SortAs", "Acronym", "str"]
-
-/* for movement of text by mouse on canvas */
-/*var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
-//variables
-var $canvas = $("#canvas");
-var canvasOffset = $canvas.offset();
-var offsetX = canvasOffset.left;
-var offsetY = canvasOffset.top;
-var scrollX = $canvas.scrollLeft();
-var scrollY = $canvas.scrollTop();
-var startX;
-var startY;
-var texts = [];
-var selectedText = -1;
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < texts.length; i++) {
-    var text = texts[i];
-    ctx.fillText(text.text, text.x, text.y);
-  }
-}
-
-function textHittest(x, y, textIndex) {
-  var text = texts[textIndex];
-  return (x >= text.x && x <= text.x + text.width && y >= text.y - text.height && y <= text.y);
-}
-
-function handleMouseDown(e) {
-  e.preventDefault();
-  startX = parseInt(e.clientX - offsetX);
-  startY = parseInt(e.clientY - offsetY);
-  // Put your mousedown stuff here
-  for (var i = 0; i < texts.length; i++) {
-    if (textHittest(startX, startY, i)) {
-      selectedText = i;
-    }
-  }
-}
-// done dragging
-function handleMouseUp(e) {
-  e.preventDefault();
-  selectedText = -1;
-}
-// also done dragging
-function handleMouseOut(e) {
-  e.preventDefault();
-  selectedText = -1;
-}
-
-function handleMouseMove(e) {
-    if (selectedText < 0) {
-        return;
-    }
-    e.preventDefault();
-    mouseX = parseInt(e.clientX - offsetX);
-    mouseY = parseInt(e.clientY - offsetY);
-
-    // Put your mousemove stuff here
-    var dx = mouseX - startX;
-    var dy = mouseY - startY;
-    startX = mouseX;
-    startY = mouseY;
-
-    var text = texts[selectedText];
-    text.x += dx;
-    text.y += dy;
-    draw();
-}
-// listen for mouse events
-$("#canvas").mousedown(function (e) {
-    handleMouseDown(e);
-});
-$("#canvas").mousemove(function (e) {
-    handleMouseMove(e);
-});
-$("#canvas").mouseup(function (e) {
-    handleMouseUp(e);
-});
-$("#canvas").mouseout(function (e) {
-    handleMouseOut(e);
-});
-
-$('#drawText').click(function () {
-  //will calculate the y coordinate for this text on the canvas
-  var y = texts.length * 20 + 20;
-  //get the text from input
-  var text = {
-    text: $('#tweetElement').val(), //tweetElement is obtained parsed json file
-    x: 20,
-    y: y
-  };
-  //calculate the size of text for hit-testing purposes
-  ctx.font = '16px verdana';
-  text.width = ctx.measureText(text.text).width;
-  text.height = 16;
-  //pushes new text into texts array
-  texts.push(text);
-  //redraws everything
-  draw();
-});
 */
 //Make the DIV element draggagle:
 dragElement(document.getElementById("mydiv"));
+
+function manualText() {
+    document.getElementById("textBox").innerHTML = document.getElementById("field1").value;
+  }
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
